35374e6eaa5c2fea6ac6929e8fe01f2821d5b5c2
Artem Petrakov <3885604@mail.ru>
2023-02-20 17:28:42 +0300

    fix page-loader.js with linter

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -9,10 +9,8 @@ program
   .helpOption('-h, --help', 'display help for command')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-o --output [dir]', 'output dir (default: "/home/user/current-dir")')
-  .arguments('<url>')
-  .action((url) => {
-    
-  });
+  .arguments('<url>');
+// .action((url) => {
+// });
 
-
-  program.parse()
+program.parse();
